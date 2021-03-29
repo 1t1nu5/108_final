2a23ed9844f6b16b6804fd5d89c307448d9f576b
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 17:25:15 +0700

    สร้างตัวแปรรุบุความยาวสตริง

--- Nearest Number.cpp
+++ Nearest Number.cpp
@@ -2,8 +2,12 @@
 #include <string.h>
 int main()
 {
-	int number, digit;
+	int number, digit, length = 3;
 	char receive[8];
 	scanf("%s", receive);
+	for (int i = 7; i > 3; i++)
+	{
+		
+	}
 	return 0;
 }
