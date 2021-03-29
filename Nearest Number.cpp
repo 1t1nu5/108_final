74b092f948f0a87d68c9b5ff5b6161739d519ea1
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 18:08:46 +0700

    ลองใหม่

--- Nearest Number.cpp
+++ Nearest Number.cpp
@@ -3,55 +3,6 @@
 #include <stdlib.h>
 int main()
 {
-	int number, digit, length = 5;
-	char receive[8];
-	scanf("%s", receive);
-	for (int i = 1; i < 6; i--)
-	{
-		if (receive[i] == 32)
-		{
-			length = i+1;
-		}
-	}
-	printf("check:\n");
-	for (int i = 0; i < 8; i++)
-	{
-		printf("%d, %d\n", i, receive[i]);
-	}
-	system("pause");
-	switch (length)
-	{
-		case 1:
-			number = receive[0] - '0';
-			digit = receive[2] - '0';
-			break;
-		case 2:
-			number = (receive[0] - '0')*10;
-			number += receive[1] - '0';
-			digit = receive[3] - '0';
-			break;
-		case 3:
-			number = (receive[0] - '0')*100;
-			number += (receive[1] - '0')*10;
-			number += receive[2] - '0';
-			digit = receive[4] - '0';
-			break;
-		case 4:
-			number = (receive[0] - '0')*1000;
-			number += (receive[1] - '0')*100;
-			number += (receive[2] - '0')*10;
-			number += receive[3] - '0';
-			digit = receive[5] - '0';
-			break;
-		case 5:
-			number = (receive[0] - '0')*10000;
-			number += (receive[1] - '0')*1000;
-			number += (receive[2] - '0')*100;
-			number += (receive[3] - '0')*10;
-			number += receive[4] - '0';
-			digit = receive[6] - '0';
-			break;
-	}
-	printf("%d %d", number, digit);
+	
 	return 0;
 }
