0a0eab4166f8165ce169e9c91aa745edd64f1d4b
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 18:24:33 +0700

    เพิ่มค่าจำนวนความคิดเห็น

--- looblike.cpp
+++ looblike.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	int max, amount[10000];
+	int max, amount[10000] = {}, get;
 	scanf("%d", &max);
+	for (int i = 0; i < max; i++)
+	{
+		scanf("%d", get);
+		amount[get]++;
+	}
 	return 0;
 }
