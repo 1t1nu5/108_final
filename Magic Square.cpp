b844a6efa462cc41433a74af9d6a239865b558aa
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 18:28:34 +0700

    ลบแล้วลองคิดใหม่

--- Magic Square.cpp
+++ Magic Square.cpp
@@ -1,38 +1,6 @@
 #include <stdio.h>
 int main()
 {
-	int n0, n1, n2;
-	scanf("%d %d %d", &n0, &n1, &n2);
-	int m0[n0][n0];
-	int m1[n1][n1];
-	int m2[n2][n2];
-	for (int i = 0; i < n0; i++)
-	{
-		for (int j = 0; j < n0; j++)
-		{
-			scanf("%d", m0[i][j]);
-		}
-	}
-	for (int i = 0; i < n1; i++)
-	{
-		for (int j = 0; j < n1; j++)
-		{
-			scanf("%d", m1[i][j]);
-		}
-	}
-	for (int i = 0; i < n2; i++)
-	{
-		for (int j = 0; j < n2; j++)
-		{
-			scanf("%d", m2[i][j]);
-		}
-	}
-		
-		int test = 0, check = 0;
-		for (int i = 0; i < n0; i++)
-		{
-			test += m0[i][i];
-		}
-	romp1:
-		;
+	
+	return 0;
 }
