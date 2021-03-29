9e9b8a00c872797181666f73a691b626ef377934
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 18:05:11 +0700

    คิดใหม่

--- Magic Square.cpp
+++ Magic Square.cpp
@@ -27,46 +27,8 @@ int main()
 			scanf("%d", m2[i][j]);
 		}
 	}
-	
-		int test = m0[0][0]+m[1][1]+m[2][2];
-		if (test != m0[0][2]+m[1][1]+m[2][0])
-		{
-			printf("no\n");
-			goto romp1;
-		}
-		if (test != m0[0][0]+m[0][1]+m[0][2])
-		{
-			printf("no\n");
-			goto romp1;
-		}
-		if (test != m0[1][0]+m[1][1]+m[1][2])
-		{
-			printf("no\n");
-			goto romp1;
-		}
-		if (test != m0[2][0]+m[2][1]+m[2][2])
-		{
-			printf("no\n");
-			goto romp1;
-		}
-		if (test != m0[0][0]+m[1][0]+m[2][0])
-		{
-			printf("no\n");
-			goto romp1;
-		}
-		if (test != m0[0][1]+m[1][1]+m[2][1])
-		{
-			printf("no\n");
-			goto romp1;
-		}
-		if (test != m0[0][3]+m[1][3]+m[2][3])
-		{
-			printf("no\n");
-		}
-		else
-		{
-			printf("yes\n");
-		}
+		
+		
 	romp1:
 		
 }
