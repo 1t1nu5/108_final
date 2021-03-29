50b05e9037adca01059f4215402bc996d8ee6643
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 18:04:00 +0700

    ตรวจสอบแนวนอนและแนวตั้ง

--- Magic Square.cpp
+++ Magic Square.cpp
@@ -34,6 +34,39 @@ int main()
 			printf("no\n");
 			goto romp1;
 		}
+		if (test != m0[0][0]+m[0][1]+m[0][2])
+		{
+			printf("no\n");
+			goto romp1;
+		}
+		if (test != m0[1][0]+m[1][1]+m[1][2])
+		{
+			printf("no\n");
+			goto romp1;
+		}
+		if (test != m0[2][0]+m[2][1]+m[2][2])
+		{
+			printf("no\n");
+			goto romp1;
+		}
+		if (test != m0[0][0]+m[1][0]+m[2][0])
+		{
+			printf("no\n");
+			goto romp1;
+		}
+		if (test != m0[0][1]+m[1][1]+m[2][1])
+		{
+			printf("no\n");
+			goto romp1;
+		}
+		if (test != m0[0][3]+m[1][3]+m[2][3])
+		{
+			printf("no\n");
+		}
+		else
+		{
+			printf("yes\n");
+		}
 	romp1:
 		
 }
