94a34664d549a6a32ca1defd598a807a59cc66ff
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 17:58:22 +0700

    ใส่ค่าไปในอาเรย์สองมิติ

--- Magic Square.cpp
+++ Magic Square.cpp
@@ -3,14 +3,15 @@ int main()
 {
 	int n0, n1, n2;
 	scanf("%d %d %d", &n0, &n1, &n2);
-	int m0[n0[n0];
+	int m0[n0][n0];
 	int m1[n1][n1];
 	int m2[n2][n2];
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < n0; i++)
 	{
-		for (int j = 0; j < n[i]; j++)
+		for (int j = 0; j < n0; j++)
 		{
-			for (int k = 0; k)
+			scanf("%d", m0[i][j]);
 		}
 	}
+	
 }
