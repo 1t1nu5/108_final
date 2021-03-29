3a18e47f81ff6984ef3afff94512315f6b120cfd
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 17:56:21 +0700

    สร้าง อาเรย์สองมิติ

--- Magic Square.cpp
+++ Magic Square.cpp
@@ -3,4 +3,14 @@ int main()
 {
 	int n[3];
 	scanf("%d %d %d", &n[0], &n[1], &n[2]);
+	int m0[n[0]][n[0]];
+	int m1[n[0]][n[0]];
+	int m2[n[0]][n[0]];
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < n[i]; j++)
+		{
+			for (int k = 0; k)
+		}
+	}
 }
