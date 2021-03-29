558e9394f479e76d6def9920b3878d823579c665
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 18:59:18 +0700

    undo

--- Nearest Number.cpp
+++ Nearest Number.cpp
@@ -3,16 +3,11 @@ int main()
 {
 	int number, digit;
 	scanf("%d %d", &number, &digit);
-	if (number%10 == digit)
-	{
-		printf("%d", number);
-		return 0;
-	}
-	do
+	number++;
+	while (number%10 != digit)
 	{
 		number++;
 	}
-	while (number%10 != digit);
 	printf("%d", number);
 	return 0;
 }
