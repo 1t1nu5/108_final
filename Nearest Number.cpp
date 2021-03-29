84635150b10af79dd2fec0b8dc84cf9097140e76
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 17:26:30 +0700

    ตรวจสอบความยาวสตริง

--- Nearest Number.cpp
+++ Nearest Number.cpp
@@ -3,11 +3,14 @@
 int main()
 {
 	int number, digit, length = 3;
-	char receive[8];
+	char receive[8] = {};
 	scanf("%s", receive);
-	for (int i = 7; i > 3; i++)
+	for (int i = 6; i > 3; i--)
 	{
-		
+		if (receive[i] == NULL)
+		{
+			length = i+1;
+		}
 	}
 	return 0;
 }
