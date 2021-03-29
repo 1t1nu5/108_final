0d78f945e2dafa16812466b56d9c10c7731fe9c1
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 17:54:33 +0700

    รับตัวแปร

--- Magic Square.cpp
+++ Magic Square.cpp
@@ -1,12 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 int main()
 {
-	char msg[10];
-	scanf("%s", msg);
-	for (int i = 0; i < 10; i++)
-	{
-		printf("%d %d\n", i, msg[i]);
-	}
-	return 0;
+	int n[3];
+	scanf("%d %d %d", &n[0], &n[1], &n[2]);
 }
