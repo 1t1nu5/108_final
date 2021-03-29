a03b2b494395b15b8f55c7ed8c84147d9d7e896a
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-29 17:59:00 +0700

    รับค่าทั้งสามชุด

--- Magic Square.cpp
+++ Magic Square.cpp
@@ -13,5 +13,18 @@ int main()
 			scanf("%d", m0[i][j]);
 		}
 	}
-	
+	for (int i = 0; i < n1; i++)
+	{
+		for (int j = 0; j < n1; j++)
+		{
+			scanf("%d", m1[i][j]);
+		}
+	}
+	for (int i = 0; i < n2; i++)
+	{
+		for (int j = 0; j < n2; j++)
+		{
+			scanf("%d", m2[i][j]);
+		}
+	}
 }
